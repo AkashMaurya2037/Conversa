d463c0a625902b046fbbb4d6be29a9ea66273b42
AkashMaurya2037 <akashmaurya2037@gmail.com>
2023-06-22 19:21:35 +0530

    Initial Commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import "./App.css";
 
-function App() {
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+import Cookies from "universal-cookie";
 
+import { ChannelContainer, ChannelListContainer } from "./components";
+
+const apiKey = "tmz33vp456eb";
+
+const client = StreamChat.getInstance(apiKey);
+
+const App = () => {
   return (
     <>
-      <div>
-        <h1>PAger</h1>
+      <div className="app__wrapper">
+        <Chat client={client} theme="team light">
+          <ChannelListContainer />
+          <ChannelContainer />
+        </Chat>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ client/src/components/ChannelContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ChannelContainer = () => {
+  return (
+    <>
+      <div>Container</div>
+    </>
+  )
+}
+
+export default ChannelContainer
